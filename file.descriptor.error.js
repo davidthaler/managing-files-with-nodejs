3a72a68a6a4da62dfbce120e6d207a494ac9bc26
David Thaler <davidthaler@gmail.com>
2020-10-25 19:43:39 -0700

    tinkering with closing

--- file.descriptor.error.js
+++ file.descriptor.error.js
@@ -3,4 +3,7 @@ const fs = require("fs");
 console.log('Opening files...');
 for (let i = 0; i < 50000; i++) {
   const fd = fs.openSync("./data/app.log");
+  console.log(fd)
+  // without this, program crashes with too many open files
+  fs.closeSync(fd)
 }
