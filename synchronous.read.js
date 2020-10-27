2122c1ca93cd8ab3bbe330f0e0b7dda1df677d3c
David Thaler <davidthaler@gmail.com>
2020-10-26 21:08:08 -0700

    folder re-org

--- synchronous.read.js
+++
@@ -1,5 +0,0 @@
-const { convertCsv } = require('./csv.parse')
-const { readFileSync } = require('fs')
-
-const data = readFileSync('./data/pulitzer-circulation-data.csv', 'utf8')
-console.table(convertCsv(data))
